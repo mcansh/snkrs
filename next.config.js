b0df040682235e939d4798972b55448b1c8eb793
Logan McAnsh <logan@mcan.sh>
2020-05-12 17:03:18 -0400

    feat: add rewrite to sw
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- next.config.js
+++ next.config.js
@@ -22,6 +22,12 @@ const nextConfig = {
     redirects: () => [
       { source: '/shoes/:id', destination: '/sneakers/:id', permanent: true },
     ],
+    rewrites: () => [
+      {
+        source: '/sw.js',
+        destination: '/_next/static/sw.js',
+      },
+    ],
   },
   env: {
     VERSION: require('./package.json').version,
