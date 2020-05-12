78f662c12a58d003b231c4c24f0e9796c1e6362e
Logan McAnsh <logan@mcan.sh>
2020-05-12 16:59:58 -0400

    feat: svg favicon
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { DefaultSeo } from 'next-seo';
+import Head from 'next/head';
 
 import { useServiceWorker } from 'src/hooks/use-service-worker';
 
@@ -20,6 +21,12 @@ const Layout: React.FC = ({ children }) => {
           site: '@loganmcansh',
         }}
       />
+      <Head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👟</text></svg>"
+        />
+      </Head>
       {children}
     </>
   );
