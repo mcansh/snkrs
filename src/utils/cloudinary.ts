a5e30db33a5af26cf1389387f11f557caed743e2
Logan McAnsh <logan@mcan.sh>
2020-05-12 20:16:18 -0400

    fix: correctly set cloudinary transforms
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -2,10 +2,15 @@ import { Cloudinary, Transformation } from 'cloudinary-core';
 
 const cloudinary = new Cloudinary({ cloud_name: 'dof0zryca', secure: true });
 
-function getCloudinaryURL(publicId: string, ...transformations: string[]) {
-  const transforms: Transformation.Options = [
-    ...new Set([...transformations, 'f_auto', 'q_auto']),
-  ];
+function getCloudinaryURL(
+  publicId: string,
+  transformations: Transformation.Options
+) {
+  const transforms: Transformation.Options = {
+    ...transformations,
+    fetchFormat: 'auto',
+    quality: 'auto',
+  };
 
   return cloudinary.url(publicId, transforms);
 }
