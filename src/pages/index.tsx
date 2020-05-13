38571cb0f7e0d08189dcf6dbe80afd4025380d68
Logan McAnsh <logan@mcan.sh>
2020-05-13 10:41:17 -0400

    style: add medium breakpoint to grid
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,7 +49,7 @@ const Index: NextPage<Props> = ({ sneakers }) => {
         Sneaker Collection – {data.length} and counting
       </h1>
 
-      <ul className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-4">
+      <ul className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {data.map(sneaker => (
           <Sneaker key={sneaker.id} {...sneaker} />
         ))}
