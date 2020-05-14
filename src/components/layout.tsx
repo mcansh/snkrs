4c2c810ceff79118123add63e54ff681a6db21db
Logan McAnsh <logan@mcan.sh>
2020-05-14 17:04:52 -0400

    feat: add favicons
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -19,12 +19,37 @@ const Layout: React.FC = ({ children }) => {
           handle: '@loganmcansh',
           site: '@loganmcansh',
         }}
+        additionalMetaTags={[
+          { name: 'apple-mobile-web-app-title', content: 'Sneakers' },
+          { name: 'application-name', content: 'Sneakers' },
+          { name: 'msapplication-TileColor', content: '#000000' },
+          { name: 'theme-color', content: '#f7f7f7' },
+        ]}
       />
       <Head>
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👟</text></svg>"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="alternate icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="alternate icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
       </Head>
       {children}
     </>
