8ec8fd894fa38c7febc1e153e9fe7e1e99de79c8
Logan McAnsh <logan@mcan.sh>
2020-06-28 15:17:00 -0400

    fix(lint-staged): use proper syntax
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,7 +3,7 @@ const { quote: escape } = require('shell-quote');
 const isWin = process.platform === 'win32';
 
 module.exports = {
-  '**/*.{js|jsx|ts|tsx}': filenames => {
+  '**/*.{js,jsx,ts,tsx}': filenames => {
     const escapedFileNames = filenames
       .map(filename => `"${isWin ? filename : escape([filename])}"`)
       .join(' ');
@@ -14,7 +14,7 @@ module.exports = {
       `jest --bail --findRelatedTests ${escapedFileNames}`,
     ];
   },
-  '**/*.{json|yml|yaml|css|less|scss|md|graphql|mdx}': filenames => {
+  '**/*.{json,yml,yaml,css,less,scss,md,graphql,mdx}': filenames => {
     const escapedFileNames = filenames
       .map(filename => `"${isWin ? filename : escape([filename])}"`)
       .join(' ');
