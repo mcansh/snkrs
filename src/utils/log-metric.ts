ca33b0faa9275ab8c40313bc38b5aa04b6da8170
Logan McAnsh <logan@mcan.sh>
2020-08-04 10:45:00 -0400

    chore(log-metric): use official NextWebVitalsMetric type
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- src/utils/log-metric.ts
+++ src/utils/log-metric.ts
@@ -1,23 +1,6 @@
-export type MetricName =
-  | 'FCP'
-  | 'LCP'
-  | 'CLS'
-  | 'FID'
-  | 'TTFB'
-  | 'Next.js-hydration'
-  | 'Next.js-route-change-to-render'
-  | 'Next.js-render';
+import { NextWebVitalsMetric } from 'next/app';
 
-export type MetricLabel = 'custom' | 'web-vital';
-
-export interface Metric {
-  label: MetricLabel;
-  name: MetricName;
-  startTime: number;
-  value: number;
-}
-
-function logMetric({ name, value }: { name: MetricName; value: number }) {
+function logMetric({ name, value }: NextWebVitalsMetric) {
   const url = `https://qckm.io?m=${name}&v=${value}&k=${process.env.QUICKMETRICS_API_KEY}`;
 
   if (navigator.sendBeacon) {
