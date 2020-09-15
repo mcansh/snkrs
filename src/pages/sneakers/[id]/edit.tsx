73dfc0003927d103dbf03fb60459e452fd42231e
Logan McAnsh <logan@mcan.sh>
2020-09-14 21:56:49 -0400

    feat: add yup for client side form validation
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- src/pages/sneakers/[id]/edit.tsx
+++ src/pages/sneakers/[id]/edit.tsx
@@ -1,68 +1,81 @@
 import React from 'react';
-import { NextPage } from 'next';
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import Link from 'next/link';
 import { NextSeo } from 'next-seo';
-import { Sneaker } from '@prisma/client';
+import { Sneaker as SneakerType } from '@prisma/client';
 import { SimpleImg } from 'react-simple-img';
-import { useFormik } from 'formik';
+import { Formik } from 'formik';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import { dequal } from 'dequal';
+import { formatISO } from 'date-fns';
+import * as Yup from 'yup';
 
 import { formatMoney } from 'src/utils/format-money';
 import { getCloudinaryURL } from 'src/utils/cloudinary';
 import { formatDate } from 'src/utils/format-date';
 import { useUser } from 'src/hooks/use-user';
+import { prisma } from 'prisma/db';
 
-interface SneakerISODate extends Omit<Sneaker, 'purchaseDate' | 'soldDate'> {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  purchaseDate: string | null;
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  soldDate: string | null;
-}
+const schema = Yup.object().shape({
+  model: Yup.string().required(),
+  colorway: Yup.string().required(),
+  brand: Yup.string().required(),
+  size: Yup.number().required().min(1),
+  imagePublicId: Yup.string().required(),
+  price: Yup.number().required(),
+  retailPrice: Yup.number().required(),
+  purchaseDate: Yup.date(),
+  sold: Yup.boolean().required().default(false),
+  soldDate: Yup.date().when('sold', {
+    is: sold => sold === true,
+    then: Yup.date().required('soldDate is required'),
+  }),
+  soldPrice: Yup.number().when('sold', {
+    is: sold => sold === true,
+    then: Yup.number().required('soldPrice is required'),
+  }),
+});
+
+type Params = {
+  id: string;
+};
 
 interface Props {
-  sneaker?: SneakerISODate;
-  id?: string;
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  sneaker: SneakerType | null;
 }
 
-const SneakerPage: NextPage<Props> = () => {
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+  const sneakers = await prisma.sneaker.findMany({ select: { id: true } });
+
+  return {
+    fallback: 'unstable_blocking',
+    paths: sneakers.map(sneaker => ({ params: { id: sneaker.id } })),
+  };
+};
+
+export const getStaticProps: GetStaticProps<Props, Params> = async ({
+  params,
+}) => {
+  if (!params) {
+    throw new Error('no params!');
+  }
+
+  const sneaker = await prisma.sneaker.findOne({ where: { id: params.id } });
+  return { props: { sneaker }, revalidate: 60 * 60 };
+};
+
+const SneakerPage: NextPage<Props> = ({ sneaker }) => {
   const router = useRouter();
   const { user } = useUser({ redirectTo: `/login?continue=${router.asPath}` });
   const { id } = router.query;
-  const { data: sneaker, error } = useSWR<SneakerISODate>(
-    id ? `/api/sneakers/${id}` : null
+  const { data, error, mutate } = useSWR<SneakerType>(
+    id ? `/api/sneakers/${id}` : null,
+    { initialData: sneaker ?? undefined }
   );
 
-  const form = useFormik({
-    enableReinitialize: true,
-    initialValues: {
-      model: sneaker?.model,
-      colorway: sneaker?.colorway,
-      brand: sneaker?.brand,
-      size: sneaker?.size,
-      imagePublicId: sneaker?.imagePublicId,
-      price: sneaker?.price,
-      retailPrice: sneaker?.retailPrice,
-      purchaseDate: sneaker?.purchaseDate?.slice(0, 16),
-      sold: sneaker?.sold,
-      soldDate: sneaker?.soldDate?.slice(0, 16),
-      soldPrice: sneaker?.soldPrice,
-    },
-    onSubmit: async values => {
-      const promise = await fetch(`/api/sneakers/${id}/edit`, {
-        method: 'PATCH',
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify(values),
-      });
-
-      if (promise.ok) {
-        router.push('/sneakers/[id]', `/sneakers/${id}`);
-      }
-    },
-  });
-
-  if (!user || (!sneaker && !error)) {
+  if (!user || (!data && !error)) {
     return (
       <div className="flex items-center justify-center w-full h-full font-mono text-lg text-center">
         loading...
@@ -108,20 +121,6 @@ const SneakerPage: NextPage<Props> = () => {
   const image2x = getCloudinaryURL(imagePublicId, { width: 800, crop: 'pad' });
   const image3x = getCloudinaryURL(imagePublicId, { width: 1200, crop: 'pad' });
 
-  const valuesAreEqual = dequal(form.values, {
-    model: sneaker.model,
-    colorway: sneaker.colorway,
-    brand: sneaker.brand,
-    size: sneaker.size,
-    imagePublicId: sneaker.imagePublicId,
-    price: sneaker.price,
-    retailPrice: sneaker.retailPrice,
-    purchaseDate: sneaker.purchaseDate?.slice(0, 16),
-    sold: sneaker.sold,
-    soldDate: sneaker.soldDate?.slice(0, 16),
-    soldPrice: sneaker.soldPrice,
-  });
-
   return (
     <main className="container min-h-full p-4 mx-auto">
       <NextSeo
@@ -154,7 +153,10 @@ const SneakerPage: NextPage<Props> = () => {
           <p className="text-xl">{formatMoney(sneaker.price)}</p>
           {sneaker.purchaseDate && (
             <p>
-              <time className="text-md" dateTime={sneaker.purchaseDate}>
+              <time
+                className="text-md"
+                dateTime={sneaker.purchaseDate.toISOString()}
+              >
                 Purchased {formatDate(sneaker.purchaseDate)}
               </time>
             </p>
@@ -163,102 +165,179 @@ const SneakerPage: NextPage<Props> = () => {
       </div>
       <div>
         <h2 className="py-4 text-lg">Edit Sneaker:</h2>
-        <form className="space-y-4" onSubmit={form.handleSubmit}>
-          <div className="grid items-center gap-2 sm:grid-cols-2">
-            <input
-              className="p-1 border-2 border-gray-200 rounded appearance-none"
-              type="text"
-              value={form.values.brand}
-              onChange={form.handleChange}
-              placeholder="Brand"
-              name="brand"
-            />
-            <input
-              className="p-1 border-2 border-gray-200 rounded appearance-none"
-              type="text"
-              value={form.values.model}
-              onChange={form.handleChange}
-              placeholder="Model"
-              name="model"
-            />
-            <input
-              className="p-1 border-2 border-gray-200 rounded appearance-none"
-              type="text"
-              value={form.values.colorway}
-              onChange={form.handleChange}
-              placeholder="Colorway"
-              name="colorway"
-            />
-            <input
-              className="p-1 border-2 border-gray-200 rounded appearance-none"
-              type="number"
-              value={form.values.price}
-              onChange={form.handleChange}
-              placeholder="Price"
-              name="price"
-            />
-            <input
-              className="p-1 border-2 border-gray-200 rounded appearance-none"
-              type="number"
-              value={form.values.retailPrice}
-              onChange={form.handleChange}
-              placeholder="Retail Price"
-              name="retailPrice"
-            />
-            <input
-              className="p-1 border-2 border-gray-200 rounded appearance-none"
-              type="datetime-local"
-              value={form.values.purchaseDate}
-              onChange={form.handleChange}
-              placeholder="Purchase Date"
-              name="purchaseDate"
-            />
-            <div
-              className="grid items-center w-full gap-2 sm:grid-cols-2 grid-col"
-              style={{
-                gridColumn: '1/3',
-                paddingTop: !form.values.sold ? 6 : undefined,
-              }}
-            >
-              <label className="flex items-center justify-between">
-                <span className="">Sold?</span>
-                <input
-                  type="checkbox"
-                  checked={form.values.sold}
-                  name="sold"
-                  onChange={form.handleChange}
-                />
-              </label>
-              {form.values.sold && (
-                <>
+        <Formik
+          validationSchema={schema}
+          initialValues={{
+            model: sneaker.model,
+            colorway: sneaker.colorway,
+            brand: sneaker.brand,
+            size: sneaker.size,
+            imagePublicId: sneaker.imagePublicId,
+            price: sneaker.price,
+            retailPrice: sneaker.retailPrice,
+            purchaseDate: sneaker.purchaseDate
+              ? formatISO(sneaker.purchaseDate, { representation: 'date' })
+              : undefined,
+            sold: sneaker.sold,
+            soldDate: sneaker.soldDate
+              ? formatISO(sneaker.soldDate, { representation: 'date' })
+              : undefined,
+            soldPrice: sneaker?.soldPrice ?? undefined,
+          }}
+          onSubmit={async values => {
+            const promise = await fetch(`/api/sneakers/${id}/edit`, {
+              method: 'PATCH',
+              headers: { 'content-type': 'application/json' },
+              body: JSON.stringify(values),
+            });
+
+            mutate({
+              ...values,
+              id: sneaker.id,
+              stockxProductId: sneaker.stockxProductId ?? null,
+              purchaseDate: values.purchaseDate
+                ? new Date(values.purchaseDate)
+                : sneaker.purchaseDate,
+              soldDate: values.soldDate
+                ? new Date(values.soldDate)
+                : sneaker.soldDate,
+              soldPrice: values.soldPrice ?? null,
+              userId: sneaker.userId,
+            });
+
+            if (promise.ok) {
+              router.push('/sneakers/[id]', `/sneakers/${id}`);
+            }
+          }}
+        >
+          {form => {
+            const valuesAreEqual = dequal(form.values, {
+              model: sneaker.model,
+              colorway: sneaker.colorway,
+              brand: sneaker.brand,
+              size: sneaker.size,
+              imagePublicId: sneaker.imagePublicId,
+              price: sneaker.price,
+              retailPrice: sneaker.retailPrice,
+              purchaseDate: sneaker.purchaseDate
+                ? formatISO(sneaker.purchaseDate)
+                : undefined,
+              sold: sneaker.sold,
+              soldDate: sneaker.soldDate
+                ? formatISO(sneaker.soldDate)
+                : undefined,
+              soldPrice: sneaker.soldPrice,
+            });
+
+            return (
+              <form className="space-y-4" onSubmit={form.handleSubmit}>
+                {form.errors && (
+                  <ul className="font-mono text-sm text-red-600 list-disc list-inside">
+                    {Object.values(form.errors).map(errorMessage => (
+                      <li key={errorMessage}>{errorMessage}</li>
+                    ))}
+                  </ul>
+                )}
+                <div className="grid items-center gap-2 sm:grid-cols-2">
                   <input
                     className="p-1 border-2 border-gray-200 rounded appearance-none"
-                    type="datetime-local"
-                    value={form.values.soldDate}
+                    type="text"
+                    value={form.values.brand}
                     onChange={form.handleChange}
-                    placeholder="Sold Date"
-                    name="soldDate"
+                    placeholder="Brand"
+                    name="brand"
+                  />
+                  <input
+                    className="p-1 border-2 border-gray-200 rounded appearance-none"
+                    type="text"
+                    value={form.values.model}
+                    onChange={form.handleChange}
+                    placeholder="Model"
+                    name="model"
+                  />
+                  <input
+                    className="p-1 border-2 border-gray-200 rounded appearance-none"
+                    type="text"
+                    value={form.values.colorway}
+                    onChange={form.handleChange}
+                    placeholder="Colorway"
+                    name="colorway"
                   />
                   <input
                     className="p-1 border-2 border-gray-200 rounded appearance-none"
                     type="number"
-                    value={form.values.soldPrice ?? sneaker.price}
+                    value={form.values.price}
                     onChange={form.handleChange}
-                    placeholder="Sold Price"
-                    name="soldPrice"
+                    placeholder="Price"
+                    name="price"
                   />
-                </>
-              )}
-            </div>
-          </div>
-          <button
-            disabled={!form.isValid || form.isSubmitting || valuesAreEqual}
-            type="submit"
-            className="self-start w-auto px-4 py-2 text-left text-white bg-blue-500 rounded disabled:bg-blue-200 disabled:cursor-not-allowed"
-          >
-            Sav{form.isSubmitting ? 'ing' : 'e'} Changes
-          </button>
-        </form>
+                  <input
+                    className="p-1 border-2 border-gray-200 rounded appearance-none"
+                    type="number"
+                    value={form.values.retailPrice}
+                    onChange={form.handleChange}
+                    placeholder="Retail Price"
+                    name="retailPrice"
+                  />
+                  <input
+                    className="p-1 border-2 border-gray-200 rounded appearance-none"
+                    type="datetime-local"
+                    value={form.values.purchaseDate}
+                    onChange={form.handleChange}
+                    placeholder="Purchase Date"
+                    name="purchaseDate"
+                  />
+                  <div
+                    className="grid items-center w-full gap-2 sm:grid-cols-2 grid-col"
+                    style={{
+                      gridColumn: '1/3',
+                      paddingTop: !form.values.sold ? 6 : undefined,
+                    }}
+                  >
+                    <label className="flex items-center justify-between">
+                      <span className="">Sold?</span>
+                      <input
+                        type="checkbox"
+                        checked={form.values.sold}
+                        name="sold"
+                        onChange={form.handleChange}
+                      />
+                    </label>
+                    {form.values.sold && (
+                      <>
+                        <input
+                          className="p-1 border-2 border-gray-200 rounded appearance-none"
+                          type="datetime-local"
+                          value={form.values.soldDate}
+                          onChange={form.handleChange}
+                          placeholder="Sold Date"
+                          name="soldDate"
+                        />
+                        <input
+                          className="p-1 border-2 border-gray-200 rounded appearance-none"
+                          type="number"
+                          value={form.values.soldPrice ?? sneaker.price}
+                          onChange={form.handleChange}
+                          placeholder="Sold Price"
+                          name="soldPrice"
+                        />
+                      </>
+                    )}
+                  </div>
+                </div>
+                <button
+                  disabled={
+                    !form.isValid || form.isSubmitting || valuesAreEqual
+                  }
+                  type="submit"
+                  className="self-start w-auto px-4 py-2 text-left text-white bg-blue-500 rounded disabled:bg-blue-200 disabled:cursor-not-allowed"
+                >
+                  Sav{form.isSubmitting ? 'ing' : 'e'} Changes
+                </button>
+              </form>
+            );
+          }}
+        </Formik>
       </div>
     </main>
   );
