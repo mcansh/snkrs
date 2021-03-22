6c4cca2447ad3d21fa9e7417f1e401c3828e0463
Logan McAnsh <logan@mcan.sh>
2021-03-21 20:31:11 -0400

    chore(deps): bump to latest
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { createRequestHandler } = require('@remix-run/vercel');
 const { PrismaClient } = require('@prisma/client');
+const remixConfig = require('./remix.config');
 
 const prisma = new PrismaClient();
 
 module.exports = createRequestHandler({
-  build: require('./build'),
+  build: require(remixConfig.serverBuildDirectory),
   getLoadContext() {
     return { prisma };
   },
