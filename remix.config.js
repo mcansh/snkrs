08bc6c5df76809786a2d76a049e4a14406c001ed
Logan McAnsh <logan@mcan.sh>
2021-04-22 15:57:49 -0400

    chore: revert server/build
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- remix.config.js
+++ remix.config.js
@@ -2,6 +2,6 @@ module.exports = {
   appDirectory: './app',
   browserBuildDirectory: './public/build',
   publicPath: '/build/',
-  serverBuildDirectory: './server/build',
+  serverBuildDirectory: './build',
   devServerPort: 8002,
 };
