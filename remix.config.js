0500db711e81f3bd29501689161ca364ad89a60d
Logan McAnsh <logan@mcan.sh>
2021-04-22 15:00:06 -0400

    chore(deps): bump to latest
    move server build directory to server/build
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- remix.config.js
+++ remix.config.js
@@ -2,6 +2,6 @@ module.exports = {
   appDirectory: './app',
   browserBuildDirectory: './public/build',
   publicPath: '/build/',
-  serverBuildDirectory: './build',
+  serverBuildDirectory: './server/build',
   devServerPort: 8002,
 };
