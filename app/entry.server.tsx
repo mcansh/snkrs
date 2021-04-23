5c27adcfd3499bf2a66b9fb47298095cc47205d4
Logan McAnsh <logan@mcan.sh>
2021-04-22 21:19:41 -0400

    feat(entry.server): add security headers (#22)
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -3,6 +3,21 @@ import { renderToString } from 'react-dom/server';
 import { RemixServer } from '@remix-run/react';
 import type { EntryContext } from '@remix-run/node';
 
+// https://securityheaders.com
+const cspSettings = {
+  'default-src': ["'self'"],
+  'img-src': ["'self'", 'data:', 'https://images.mcan.sh/upload/'],
+  'style-src-elem': ["'self'", 'https://rsms.me/inter/inter.css'],
+  'font-src': ['https://rsms.me/inter/font-files/'],
+  'script-src': ["'self'", "'unsafe-inline'", 'https://kiwi.mcan.sh/script.js'],
+  'connect-src':
+    process.env.NODE_ENV === 'production' ? [] : ['ws://localhost:3001'],
+};
+
+const contentSecurityPolicy = `${Object.entries(cspSettings)
+  .map(([key, val]) => `${key} ${val.filter(Boolean).join(' ')}`)
+  .join(';')}`;
+
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
@@ -18,6 +33,21 @@ export default function handleRequest(
     headers: {
       ...Object.fromEntries(responseHeaders),
       'Content-Type': 'text/html',
+      // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+      'Content-Security-Policy': contentSecurityPolicy,
+      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+      'Referrer-Policy': 'origin-when-cross-origin',
+      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+      'X-Frame-Options': 'DENY',
+      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+      'X-Content-Type-Options': 'nosniff',
+      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+      'X-DNS-Prefetch-Control': 'on',
+      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+      'Strict-Transport-Security':
+        'max-age=31536000; includeSubDomains; preload',
+      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+      'Permissions-Policy': 'camera=(), microphone=(), geolocation=()',
     },
   });
 }
