9a801a166b8df5d50b993b737f18329355dd9d3a
Logan McAnsh <logan@mcan.sh>
2021-04-23 20:50:18 -0400

    feat: add really basic landing page header
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,30 @@
-import type { LoaderFunction } from '@remix-run/node';
+import * as React from 'react';
+import type { LinksFunction, LoaderFunction } from '@remix-run/node';
 import { redirect } from '@remix-run/node';
+import { Popover, Transition } from '@headlessui/react';
+import { block, Link } from '@remix-run/react';
 
+import x from '../icons/outline/x.svg';
+import menu from '../icons/outline/menu.svg';
 import { flashMessageKey, sessionKey } from '../constants';
 import { prisma } from '../db';
 import { flashMessage } from '../flash-message';
 import { withSession } from '../lib/with-session';
+
+const links: LinksFunction = () => [
+  block({
+    rel: 'preload',
+    href: x,
+    as: 'image',
+    type: 'image/svg+xml',
+  }),
+  block({
+    rel: 'preload',
+    href: menu,
+    as: 'image',
+    type: 'image/svg+xml',
+  }),
+];
 
 const loader: LoaderFunction = ({ request }) =>
   withSession(request, async session => {
@@ -18,13 +38,235 @@ const loader: LoaderFunction = ({ request }) =>
       }
 
       session.flash(flashMessageKey, flashMessage('User not found', 'error'));
-      return redirect(`/loganmcansh`);
+      return {};
     }
 
-    return redirect(`/loganmcansh`);
+    return {};
   });
 
-const IndexPage = () => null;
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+];
+
+const IndexPage = () => (
+  <div className="relative overflow-hidden bg-gray-50">
+    <div
+      className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full"
+      aria-hidden="true"
+    >
+      <div className="relative h-full mx-auto max-w-7xl">
+        <svg
+          className="absolute transform right-full translate-y-1/4 translate-x-1/4 lg:translate-x-1/2"
+          width={404}
+          height={784}
+          fill="none"
+          viewBox="0 0 404 784"
+        >
+          <defs>
+            <pattern
+              id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x={0}
+                y={0}
+                width={4}
+                height={4}
+                className="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect
+            width={404}
+            height={784}
+            fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+          />
+        </svg>
+        <svg
+          className="absolute transform left-full -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
+          width={404}
+          height={784}
+          fill="none"
+          viewBox="0 0 404 784"
+        >
+          <defs>
+            <pattern
+              id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x={0}
+                y={0}
+                width={4}
+                height={4}
+                className="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect
+            width={404}
+            height={784}
+            fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"
+          />
+        </svg>
+      </div>
+    </div>
+
+    <Popover className="relative pt-6 pb-16 sm:pb-24">
+      {({ open }) => (
+        <>
+          <div className="px-4 mx-auto max-w-7xl sm:px-6">
+            <nav
+              className="relative flex items-center justify-between sm:h-10 md:justify-center"
+              aria-label="Global"
+            >
+              <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
+                <div className="flex items-center justify-between w-full md:w-auto">
+                  <a href="/">
+                    <span className="sr-only">Sneakers</span>
+                    <img
+                      className="w-auto h-8 rounded-full sm:h-10"
+                      src="/apple-touch-icon.png"
+                      alt=""
+                    />
+                  </a>
+                  <div className="flex items-center -mr-2 md:hidden">
+                    <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md bg-gray-50 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                      <span className="sr-only">Open main menu</span>
+                      <svg className="w-6 h-6" aria-hidden="true">
+                        <use href={`${menu}#menu`} />
+                      </svg>
+                    </Popover.Button>
+                  </div>
+                </div>
+              </div>
+              <div className="hidden md:flex md:space-x-10">
+                {navigation.map(item => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="font-medium text-gray-500 hover:text-gray-900"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
+                <span className="inline-flex rounded-md shadow">
+                  <Link
+                    to="/login"
+                    className="inline-flex items-center px-4 py-2 text-base font-medium text-indigo-600 bg-white border border-transparent rounded-md hover:bg-gray-50"
+                  >
+                    Log in
+                  </Link>
+                </span>
+              </div>
+            </nav>
+          </div>
+
+          <Transition
+            show={open}
+            as={React.Fragment}
+            enter="duration-150 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Popover.Panel
+              focus
+              static
+              className="absolute inset-x-0 top-0 p-2 transition origin-top-right transform md:hidden"
+            >
+              <div className="overflow-hidden bg-white rounded-lg shadow-md ring-1 ring-black ring-opacity-5">
+                <div className="flex items-center justify-between px-5 pt-4">
+                  <div>
+                    <img
+                      className="w-auto h-8"
+                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="-mr-2">
+                    <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                      <span className="sr-only">Close menu</span>
+                      <svg className="w-6 h-6" aria-hidden="true">
+                        <use href={`${x}#x`} />
+                      </svg>
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="px-2 pt-2 pb-3">
+                  {navigation.map(item => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="block px-3 py-2 text-base font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <Link
+                  to="/login"
+                  className="block w-full px-5 py-3 font-medium text-center text-indigo-600 bg-gray-50 hover:bg-gray-100"
+                >
+                  Log in
+                </Link>
+              </div>
+            </Popover.Panel>
+          </Transition>
+
+          <main className="px-4 mx-auto mt-16 max-w-7xl sm:mt-24">
+            <div className="text-center">
+              <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+                <span className="block xl:inline">
+                  Show off your sneaker collection
+                </span>
+              </h1>
+              <p className="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+                You worked hard and spent money on your shoes, why not show them
+                off
+              </p>
+              <div className="max-w-md mx-auto mt-5 sm:flex sm:justify-center md:mt-8">
+                <div className="rounded-md shadow">
+                  <Link
+                    to="/join"
+                    className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                  >
+                    Get started
+                  </Link>
+                </div>
+                <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+                  <Link
+                    to="/loganmcansh"
+                    className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-indigo-600 bg-white border border-transparent rounded-md hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+                  >
+                    Live demo
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </main>
+        </>
+      )}
+    </Popover>
+  </div>
+);
 
 export default IndexPage;
-export { loader };
+export { links, loader };
---
+++ scripts/heroicon-symbols.mjs
@@ -0,0 +1,72 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import path from 'path';
+import { promises as fs, constants } from 'fs';
+
+import { optimize, extendDefaultPlugins } from 'svgo';
+
+const HEROCIONS_PATH = path.join(process.cwd(), 'node_modules/heroicons');
+const HEROCIONS_SOLID_PATH = path.join(HEROCIONS_PATH, 'solid');
+const HEROCIONS_OUTLINE_PATH = path.join(HEROCIONS_PATH, 'outline');
+
+const OUTDIR = path.join(process.cwd(), 'app/icons');
+const OUTDIR_SOLID = path.join(OUTDIR, 'solid');
+const OUTDIR_OUTLINE = path.join(OUTDIR, 'outline');
+
+async function createDirIfNeeded(dir) {
+  try {
+    await fs.access(dir, constants.F_OK);
+    return;
+  } catch (error) {
+    await fs.mkdir(dir);
+  }
+}
+
+async function wrapSymbol(inputPath, outputDir) {
+  const ext = path.extname(inputPath);
+  const base = path.basename(inputPath, ext);
+  const content = await fs.readFile(inputPath, 'utf-8');
+  const outputPath = path.join(outputDir, `${base}.svg`);
+
+  const result = optimize(content, {
+    path: inputPath,
+    plugins: extendDefaultPlugins([
+      {
+        name: 'removeViewBox',
+        active: false,
+      },
+      {
+        name: 'removeDimensions',
+        active: true,
+      },
+    ]),
+  });
+
+  const optimizedSvgString = result.data;
+
+  return fs.writeFile(outputPath, optimizedSvgString);
+}
+
+async function compile() {
+  // 1. verify all output directories exist
+  await createDirIfNeeded(OUTDIR);
+  await createDirIfNeeded(OUTDIR_SOLID);
+  await createDirIfNeeded(OUTDIR_OUTLINE);
+
+  // 2. get all svg icons from heroicons
+  const [solid, outline] = await Promise.all([
+    fs.readdir(HEROCIONS_SOLID_PATH),
+    fs.readdir(HEROCIONS_OUTLINE_PATH),
+  ]);
+
+  // 3. generate icons
+  await Promise.all([
+    ...solid.map(icon =>
+      wrapSymbol(path.join(HEROCIONS_SOLID_PATH, icon), OUTDIR_SOLID)
+    ),
+    ...outline.map(icon =>
+      wrapSymbol(path.join(HEROCIONS_OUTLINE_PATH, icon), OUTDIR_OUTLINE)
+    ),
+  ]);
+}
+
+compile();
