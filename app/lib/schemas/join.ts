a1f674b970b241e48acb40d7565671bc92a21549
Logan McAnsh <logan@mcan.sh>
2021-04-25 19:41:30 -0400

    feat: prevent commonly used reserved names from being used as usernames
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    (cherry picked from commit 72cc034413ab08281cad196bf6b4064ec15edaa0)

--- app/lib/schemas/join.ts
+++ app/lib/schemas/join.ts
@@ -1,11 +1,18 @@
 import * as Yup from 'yup';
+import reservedEmailAddressesList from 'reserved-email-addresses-list/index.json';
+import reservedAdminList from 'reserved-email-addresses-list/admin-list.json';
 
 const registerSchema = Yup.object().shape({
   email: Yup.string().email().required(),
   givenName: Yup.string().required(),
   familyName: Yup.string().required(),
   password: Yup.string().min(12).required(),
-  username: Yup.string().required(),
+  username: Yup.string()
+    .required()
+    .notOneOf(
+      [...reservedEmailAddressesList, ...reservedAdminList],
+      'A user with this username already exists'
+    ),
 });
 
 export { registerSchema };
