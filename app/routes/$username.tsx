57d5085ed6864fcd023462712654e2b55d406c1d
Logan McAnsh <logan@mcan.sh>
2021-04-29 14:57:18 -0400

    feat: upgrade ugly navbar/side bar situation
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/$username.tsx
+++ app/routes/$username.tsx
@@ -3,48 +3,39 @@ import type {
   RouteComponent,
   LoaderFunction,
   LinksFunction,
+  MetaFunction,
 } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { block, useRouteData } from '@remix-run/react';
-import type { Brand, User } from '@prisma/client';
-import { Link, Outlet } from 'react-router-dom';
+import { useRouteData } from '@remix-run/react';
+import type { Brand, Sneaker, User } from '@prisma/client';
 import uniqBy from 'lodash.uniqby';
-import { Dialog, Transition } from '@headlessui/react';
+import { Disclosure } from '@headlessui/react';
 
-import x from '../icons/outline/x.svg';
-import menuAlt2 from '../icons/outline/menu-alt-2.svg';
 import { prisma } from '../db';
 import { NotFoundError } from '../errors';
+import { DataOutlet } from '../components/data-outlet';
+import x from '../icons/outline/x.svg';
+import menu from '../icons/outline/menu.svg';
 
-import FourOhFour from './404';
+import FourOhFour, { meta as fourOhFourMeta } from './404';
 
-interface RouteData {
+export interface RouteData {
   brands: Array<Brand>;
   user: Pick<User, 'username' | 'id'> & {
     fullName: string;
+    possessive: string;
+    sneakers: Array<Sneaker & { brand: Brand }>;
   };
   selectedBrands: Array<string>;
   sort?: 'asc' | 'desc';
 }
 
-const links: LinksFunction = () => [
-  block({
-    rel: 'preload',
-    href: x,
-    as: 'image',
-    type: 'image/svg+xml',
-  }),
-  block({
-    rel: 'preload',
-    href: menuAlt2,
-    as: 'image',
-    type: 'image/svg+xml',
-  }),
-];
-
 const loader: LoaderFunction = async ({ params, request }) => {
   try {
     const { searchParams } = new URL(request.url);
+
+    const selectedBrands = searchParams.getAll('brand');
+    const sortQuery = searchParams.get('sort');
 
     const user = await prisma.user.findUnique({
       where: {
@@ -57,6 +48,9 @@ const loader: LoaderFunction = async ({ params, request }) => {
         familyName: true,
         sneakers: {
           include: { brand: true },
+          orderBy: {
+            purchaseDate: sortQuery === 'asc' ? 'asc' : 'desc',
+          },
         },
       },
     });
@@ -65,28 +59,30 @@ const loader: LoaderFunction = async ({ params, request }) => {
       throw new NotFoundError();
     }
 
+    const sneakers = user.sneakers.filter(sneaker =>
+      selectedBrands.includes(sneaker.brand.slug)
+    );
+
     const uniqueBrands = uniqBy(
       user.sneakers.map(sneaker => sneaker.brand),
       'name'
     ).sort((a, b) => a.name.localeCompare(b.name));
 
-    const selectedBrandQuery = searchParams.get('brand');
-    const selectedBrands = selectedBrandQuery
-      ? selectedBrandQuery.split(',')
-      : [];
+    const fullName = `${user.givenName} ${user.familyName}`;
+    const endsPossessive = fullName.toLowerCase().endsWith('s');
 
-    const sortQuery = searchParams.get('sort');
-    const sort =
-      sortQuery && ['asc', 'desc'].includes(sortQuery) ? sortQuery : undefined;
+    const nameEndsWithS = endsPossessive ? `${fullName}'` : `${fullName}'s`;
 
     return {
       user: {
         ...user,
-        fullName: `${user.givenName} ${user.familyName}`,
+        fullName,
+        sneakers,
+        possessive: nameEndsWithS,
       },
       brands: uniqueBrands,
       selectedBrands,
-      sort,
+      sort: sortQuery === 'asc' ? 'asc' : 'desc',
     };
   } catch (error) {
     if (error instanceof NotFoundError) {
@@ -97,6 +93,25 @@ const loader: LoaderFunction = async ({ params, request }) => {
   }
 };
 
+const links: LinksFunction = () => [];
+
+const meta: MetaFunction = ({ data }: { data: RouteData }) => {
+  if (!data.user) {
+    return fourOhFourMeta();
+  }
+
+  return {
+    title: `${data.user.possessive} Sneaker Collection`,
+    description: `${data.user.possessive} sneaker collection`,
+    'twitter:card': 'summary_large_image',
+    'twitter:site': '@loganmcansh',
+    // TODO: add support for linking your twitter account
+    'twitter:creator': '@loganmcansh',
+    'twitter:description': `${data.user.possessive} sneaker collection`,
+    // TODO: add support for user avatar
+  };
+};
+
 const sortOptions = [
   { value: 'desc', display: 'Recent first' },
   { value: 'asc', display: 'Oldest first' },
@@ -104,226 +119,172 @@ const sortOptions = [
 
 const UserSneakersPage: RouteComponent = () => {
   const data = useRouteData<RouteData>();
-  const [sidebarOpen, setSidebarOpen] = React.useState(false);
-
-  React.useEffect(() => {
-    setSidebarOpen(false);
-  }, [data.selectedBrands, data.sort]);
 
   if (!data.user) {
     return <FourOhFour />;
   }
 
-  function makeBrandLink(brand: string) {
-    let newBrandQueryItems = [];
-    if (data.selectedBrands.includes(brand)) {
-      newBrandQueryItems = data.selectedBrands.filter(i => i !== brand);
-    } else {
-      newBrandQueryItems = [...data.selectedBrands, brand];
-    }
-
-    const searchParams = new URLSearchParams();
-    if (newBrandQueryItems.length) {
-      searchParams.set('brand', newBrandQueryItems.join());
-    }
-    if (data.sort) {
-      searchParams.set('sort', data.sort);
-    }
-
-    return `?${searchParams.toString()}`;
-  }
-
-  function makeSortLink(sort: string) {
-    const searchParams = new URLSearchParams();
-    if (data.selectedBrands.length) {
-      searchParams.set('brand', data.selectedBrands.join());
-    }
-    searchParams.set('sort', sort);
-
-    return `?${searchParams.toString()}`;
-  }
-
   return (
-    <div className="h-full grid-cols-[200px,1fr] block md:grid">
-      <Transition.Root show={sidebarOpen} as={React.Fragment}>
-        <Dialog
-          as="div"
-          static
-          className="fixed inset-0 z-40 flex md:hidden"
-          open={sidebarOpen}
-          onClose={setSidebarOpen}
-        >
-          <Transition.Child
-            as={React.Fragment}
-            enter="transition-opacity ease-linear duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity ease-linear duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75" />
-          </Transition.Child>
-          <Transition.Child
-            as={React.Fragment}
-            enter="transition ease-in-out duration-300 transform"
-            enterFrom="-translate-x-full"
-            enterTo="translate-x-0"
-            leave="transition ease-in-out duration-300 transform"
-            leaveFrom="translate-x-0"
-            leaveTo="-translate-x-full"
-          >
-            <div className="relative flex flex-col flex-1 w-full max-w-xs px-4 pt-5 pb-4 font-medium text-white bg-purple-500">
-              <Transition.Child
-                as={React.Fragment}
-                enter="ease-in-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in-out duration-300"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <div className="absolute top-0 right-0 pt-2 -mr-12">
-                  <button
-                    type="button"
-                    className="flex items-center justify-center w-10 h-10 ml-1 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                    onClick={() => setSidebarOpen(false)}
-                  >
-                    <span className="sr-only">Close sidebar</span>
-                    <svg className="w-6 h-6 text-white" aria-hidden="true">
-                      <use href={`${x}#x`} />
-                    </svg>
-                  </button>
+    <div className="h-full md:grid-cols-[250px,1fr] block md:grid">
+      <Disclosure as="nav" className="bg-white shadow">
+        {({ open }) => (
+          <>
+            <div className="px-2 mx-auto max-w-7xl md:px-6 lg:px-8">
+              <div className="relative flex justify-between h-16">
+                <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
+                  {/* Mobile menu button */}
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <svg className="block w-6 h-6" aria-hidden="true">
+                        <use href={`${x}#x`} />
+                      </svg>
+                    ) : (
+                      <svg className="block w-6 h-6" aria-hidden="true">
+                        <use href={`${menu}#menu`} />
+                      </svg>
+                    )}
+                  </Disclosure.Button>
                 </div>
-              </Transition.Child>
-              <div className="flex-1 h-0 mt-5 overflow-y-auto">
-                <div className="space-y-10">
-                  <div>
-                    <p>Filter by brand</p>
-                    <ul>
-                      {data.brands.map(brand => (
-                        <li key={brand.id}>
-                          <Link
-                            to={{ search: makeBrandLink(brand.slug) }}
-                            className="flex items-center space-x-2"
-                          >
-                            <input
-                              className="rounded-full"
-                              type="checkbox"
-                              value={brand.slug}
-                              defaultChecked={data.selectedBrands.includes(
-                                brand.slug
-                              )}
-                            />
-                            <span>{brand.name}</span>
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
+                <aside className="hidden md:block">
+                  <div className="sticky top-0 px-4 py-6">
+                    <h1 className="mb-4 text-sm font-medium">
+                      {data.user.fullName}
+                    </h1>
 
-                  <div>
-                    <p>Sort by</p>
-                    <ul>
-                      {sortOptions.map(option => (
-                        <li key={option.value}>
-                          <Link
-                            to={{
-                              search: makeSortLink(option.value),
-                            }}
-                            className="space-x-2"
-                          >
-                            <input
-                              className="rounded-full"
-                              type="checkbox"
-                              value={option.value}
-                              defaultChecked={data.sort === option.value}
-                            />
-                            <span>{option.display}</span>
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
+                    <form method="get">
+                      <fieldset className="grid grid-cols-1 gap-2">
+                        <div>
+                          <div>Filter by brand:</div>
+                          <ul className="pb-4">
+                            {data.brands.map(brand => (
+                              <li key={brand.id}>
+                                <label className="flex items-center space-x-2">
+                                  <input
+                                    type="checkbox"
+                                    name="brand"
+                                    defaultChecked={data.selectedBrands.includes(
+                                      brand.slug
+                                    )}
+                                    value={brand.slug}
+                                    className="rounded-full"
+                                  />
+                                  <span>{brand.name}</span>
+                                </label>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                        <div>
+                          <div>Sort by:</div>
+                          <ul className="pb-4">
+                            {sortOptions.map(sort => (
+                              <li key={sort.value}>
+                                <label className="flex items-center space-x-2">
+                                  <input
+                                    type="radio"
+                                    name="sort"
+                                    defaultChecked={
+                                      data.sort
+                                        ? data.sort.includes(sort.value)
+                                        : false
+                                    }
+                                    value={sort.value}
+                                    className="rounded-full"
+                                  />
+                                  <span>{sort.display}</span>
+                                </label>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      </fieldset>
+                      <button
+                        type="submit"
+                        className="px-4 py-1 text-sm text-white bg-purple-600 rounded"
+                      >
+                        Apply
+                      </button>
+                    </form>
                   </div>
-                </div>
+                </aside>
               </div>
             </div>
-          </Transition.Child>
-          <div className="flex-shrink-0 w-14" aria-hidden="true">
-            {/* Dummy element to force sidebar to shrink to fit close icon */}
-          </div>
-        </Dialog>
-      </Transition.Root>
 
-      <div className="flex flex-col flex-1 overflow-hidden md:hidden">
-        <div className="relative z-10 flex flex-shrink-0 h-16 bg-white shadow">
-          <button
-            type="button"
-            className="px-4 text-gray-500 border-r border-gray-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
-            onClick={() => setSidebarOpen(true)}
-          >
-            <span className="sr-only">Open sidebar</span>
-            <svg className="w-6 h-6" aria-hidden="true">
-              <use href={`${menuAlt2}#menu-alt-2`} />
-            </svg>
-          </button>
-        </div>
-      </div>
+            <Disclosure.Panel className="md:hidden">
+              <div>
+                <div className="px-4 py-6">
+                  <h1 className="mb-4 text-sm font-medium">
+                    {data.user.fullName}
+                  </h1>
 
-      <aside className="hidden w-full px-4 py-6 font-medium text-white bg-purple-500 md:block">
-        <div className="space-y-10 md:sticky top-6">
-          <div className="space-y-2">
-            <p>Filter by brand</p>
-            <ul>
-              {data.brands.map(brand => (
-                <li key={brand.id}>
-                  <Link
-                    to={{ search: makeBrandLink(brand.slug) }}
-                    className="space-x-2"
-                  >
-                    <input
-                      className="rounded-full"
-                      type="checkbox"
-                      value={brand.slug}
-                      defaultChecked={data.selectedBrands.includes(brand.slug)}
-                    />
-                    <span>{brand.name}</span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
+                  <form method="get">
+                    <fieldset className="grid grid-cols-2 gap-2">
+                      <div>
+                        <div>Filter by brand:</div>
+                        <ul className="pb-4">
+                          {data.brands.map(brand => (
+                            <li key={brand.id}>
+                              <label className="flex items-center space-x-2">
+                                <input
+                                  type="checkbox"
+                                  name="brand"
+                                  defaultChecked={data.selectedBrands.includes(
+                                    brand.slug
+                                  )}
+                                  value={brand.slug}
+                                  className="rounded-full"
+                                />
+                                <span>{brand.name}</span>
+                              </label>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                      <div>
+                        <div>Sort by:</div>
+                        <ul className="pb-4">
+                          {sortOptions.map(sort => (
+                            <li key={sort.value}>
+                              <label className="flex items-center space-x-2">
+                                <input
+                                  type="radio"
+                                  name="sort"
+                                  defaultChecked={
+                                    data.sort
+                                      ? data.sort.includes(sort.value)
+                                      : false
+                                  }
+                                  value={sort.value}
+                                  className="rounded-full"
+                                />
+                                <span>{sort.display}</span>
+                              </label>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </fieldset>
+                    <button
+                      type="submit"
+                      className="px-4 py-1 text-sm text-white bg-purple-600 rounded"
+                    >
+                      Apply
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
 
-          <div className="space-y-2">
-            <p>Sort by</p>
-            <ul>
-              {sortOptions.map(option => (
-                <li key={option.value}>
-                  <Link
-                    to={{ search: makeSortLink(option.value) }}
-                    className="space-x-2"
-                  >
-                    <input
-                      className="rounded-full"
-                      type="checkbox"
-                      value={option.value}
-                      defaultChecked={data.sort === option.value}
-                    />
-                    <span>{option.display}</span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </aside>
-
-      <main className="w-full">
-        <Outlet />
+      <main>
+        <DataOutlet data={data} />
       </main>
     </div>
   );
 };
 
 export default UserSneakersPage;
-export { links, loader };
+export { links, loader, meta };
--- app/routes/$username/index.tsx
+++ app/routes/$username/index.tsx
@@ -1,115 +1,13 @@
 import * as React from 'react';
-import type {
-  RouteComponent,
-  LoaderFunction,
-  MetaFunction,
-} from '@remix-run/node';
-import { json } from '@remix-run/node';
-import { useRouteData } from '@remix-run/react';
-import type { Sneaker, Brand, User, Prisma } from '@prisma/client';
+import type { RouteComponent } from '@remix-run/node';
 
-import FourOhFour, { meta as fourOhFourMeta } from '../404';
-import { getCloudinaryURL } from '../../utils/cloudinary';
-import { prisma } from '../../db';
-import { NotFoundError } from '../../errors';
+import FourOhFour from '../404';
 import { SneakerCard } from '../../components/sneaker';
-
-interface RouteData {
-  user: Pick<User, 'username' | 'id'> & {
-    sneakers: Array<Sneaker & { brand: Brand }>;
-    fullName: string;
-  };
-}
-
-const loader: LoaderFunction = async ({ request, params }) => {
-  try {
-    const { searchParams } = new URL(request.url);
-
-    const sortQuery = searchParams.get('sort') as Prisma.SortOrder;
-    const brandQuery = searchParams.get('brand');
-    const brands = brandQuery ? brandQuery.split(',') : [];
-
-    const sort =
-      sortQuery && ['asc', 'desc'].includes(sortQuery) ? sortQuery : 'desc';
-
-    const user = await prisma.user.findUnique({
-      where: {
-        username: params.username,
-      },
-      select: {
-        username: true,
-        id: true,
-        givenName: true,
-        familyName: true,
-        sneakers: {
-          where: brandQuery
-            ? {
-                brand: {
-                  slug: {
-                    in: brands,
-                  },
-                },
-              }
-            : undefined,
-          include: { brand: true },
-          orderBy: {
-            purchaseDate: sort,
-          },
-        },
-      },
-    });
-
-    if (!user) {
-      throw new NotFoundError();
-    }
-
-    return {
-      user: {
-        ...user,
-        fullName: `${user.givenName} ${user.familyName}`,
-      },
-    };
-  } catch (error) {
-    if (error instanceof NotFoundError) {
-      return json({ notFound: true }, { status: 404 });
-    }
-    console.error(error);
-    return json({}, { status: 500 });
-  }
-};
-
-const meta: MetaFunction = ({ data }: { data: RouteData }) => {
-  if (!data.user) {
-    return fourOhFourMeta();
-  }
-
-  const endsPossessive = data.user.fullName.toLowerCase().endsWith('s');
-
-  const nameEndsWithS = endsPossessive
-    ? `${data.user.fullName}'`
-    : `${data.user.fullName}'s`;
-
-  const [latestCop] = data.user.sneakers;
-
-  return {
-    title: `${nameEndsWithS} Sneaker Collection`,
-    description: `${nameEndsWithS} sneaker collection`,
-    'twitter:card': 'summary_large_image',
-    'twitter:site': '@loganmcansh',
-    // TODO: add support for linking your twitter account
-    'twitter:creator': '@loganmcansh',
-    'twitter:description': `${nameEndsWithS} sneaker collection`,
-
-    // TODO: add support for user avatar, for now just link to latest purchase
-    'twitter:image': latestCop ? getCloudinaryURL(latestCop.imagePublicId) : '',
-    'twitter:image:alt': latestCop
-      ? `${latestCop.brand.name} ${latestCop.model} in the ${latestCop.colorway} colorway`
-      : '',
-  };
-};
+import { useParentRouteData } from '../../components/data-outlet';
+import type { RouteData } from '../$username';
 
 const UserSneakersChildPage: RouteComponent = () => {
-  const data = useRouteData<RouteData>();
+  const data = useParentRouteData<RouteData>();
 
   if (!data.user) {
     return <FourOhFour />;
@@ -125,4 +23,3 @@ const UserSneakersChildPage: RouteComponent = () => {
 };
 
 export default UserSneakersChildPage;
-export { loader, meta };
