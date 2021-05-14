e25036169e60993a4d1637fa5281e5c379229cd8
Logan McAnsh <logan@mcan.sh>
2021-05-13 22:05:02 -0400

    chore(deps): bump to latest
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,11 +1,4 @@
 import ReactDOM from 'react-dom';
 import { RemixBrowser } from 'remix';
 
-ReactDOM.hydrate(
-  // @ts-expect-error - @types/react-dom says the 2nd argument to ReactDOM.hydrate() must be a
-  // `Element | DocumentFragment | null` but React 16 allows you to pass the
-  // `document` object as well. This is a bug in @types/react-dom that we can
-  // safely ignore for now.
-  <RemixBrowser />,
-  document
-);
+ReactDOM.hydrate(<RemixBrowser />, document);
