f17e70f4ee6be1793810ea3e42d87162b6b9ee63
Logan McAnsh <logan@mcan.sh>
2021-05-28 13:55:50 -0400

    feat: save sessions to redis
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/session.ts
+++ app/session.ts
@@ -1,8 +1,58 @@
-import { createCookieSessionStorage } from 'remix';
+import { createSessionStorage } from 'remix';
+import Redis from 'ioredis';
+import cuid from 'cuid';
+import { addWeeks, differenceInMilliseconds } from 'date-fns';
 
-// eslint-disable-next-line @typescript-eslint/unbound-method
-const { getSession, commitSession, destroySession } =
-  createCookieSessionStorage({
+function createRedisSessionStorage({
+  cookie,
+}: {
+  cookie: Parameters<typeof createSessionStorage>['0']['cookie'];
+}) {
+  const redis = new Redis(process.env.REDIS_URL, {
+    keyPrefix: 'snkrs:',
+  });
+
+  return createSessionStorage({
+    cookie,
+    async createData(data, expires) {
+      // `expires` is a Date after which the data should be considered
+      // invalid. You could use it to invalidate the data somehow or
+      // automatically purge this record from your database.
+      const id = cuid();
+
+      const now = new Date();
+
+      const diff = expires
+        ? differenceInMilliseconds(expires, now)
+        : addWeeks(now, 2).getTime();
+
+      await redis.set(id, JSON.stringify(data), 'px', diff);
+
+      return id;
+    },
+    async readData(id) {
+      const session = await redis.get(id);
+      if (!session) return null;
+      const data = JSON.parse(session);
+      return data;
+    },
+    async updateData(id, data, expires) {
+      const now = new Date();
+
+      const diff = expires
+        ? differenceInMilliseconds(expires, now)
+        : addWeeks(now, 2).getTime();
+
+      await redis.set(id, JSON.stringify(data), 'px', diff);
+    },
+    async deleteData(id) {
+      await redis.del(id);
+    },
+  });
+}
+
+const { getSession, commitSession, destroySession } = createRedisSessionStorage(
+  {
     cookie: {
       name: '__session',
       secrets: [process.env.SESSION_PASSWORD],
@@ -12,6 +62,7 @@ const { getSession, commitSession, destroySession } =
       path: '/',
       secure: process.env.NODE_ENV === 'production',
     },
-  });
+  }
+);
 
 export { getSession, commitSession, destroySession };
