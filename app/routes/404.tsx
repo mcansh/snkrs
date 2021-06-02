456337f3fd472c0f550cef445b6bfeec2d6758da
Logan McAnsh <logan@mcan.sh>
2021-06-02 12:55:15 -0400

    chore: use monospaced font on 404 page
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/404.tsx
+++ app/routes/404.tsx
@@ -7,7 +7,7 @@ function meta() {
 }
 
 const FourOhFour: React.VFC = () => (
-  <div className="flex flex-col items-center justify-center w-full h-full">
+  <div className="flex flex-col items-center justify-center w-full h-full font-mono">
     <h1 className="text-3xl">404</h1>
     <p>Page not found</p>
   </div>
