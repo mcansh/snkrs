81fdd07ecc5dd91c543339b47a38d73c577d51dc
Logan McAnsh <logan@mcan.sh>
2021-06-02 12:49:26 -0400

    chore(EditSneakerPage): single column mobile form
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/sneakers.$sneakerId.edit.tsx
+++ app/routes/sneakers.$sneakerId.edit.tsx
@@ -276,110 +276,111 @@ const EditSneakerPage: React.VFC = () => {
       </div>
       <div>
         <h2 className="py-4 text-lg">Edit Sneaker:</h2>
-        <Form method="post" className="space-y-4">
-          <fieldset disabled={!!pendingForm}>
-            <div className="grid items-center gap-2 sm:grid-cols-2">
+        <Form method="post">
+          <fieldset
+            disabled={!!pendingForm}
+            className="pb-4 space-y-2 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-2"
+          >
+            <input
+              className="w-full p-1 border-2 border-gray-200 rounded appearance-none"
+              type="text"
+              defaultValue={sneaker.brand.name}
+              placeholder="Brand"
+              name="brand"
+            />
+            <input
+              className="w-full p-1 border-2 border-gray-200 rounded appearance-none"
+              type="text"
+              defaultValue={sneaker.model}
+              placeholder="Model"
+              name="model"
+            />
+            <input
+              className="w-full p-1 border-2 border-gray-200 rounded appearance-none"
+              type="text"
+              defaultValue={sneaker.colorway}
+              placeholder="Colorway"
+              name="colorway"
+            />
+            <input
+              className="w-full p-1 border-2 border-gray-200 rounded appearance-none"
+              type="number"
+              defaultValue={sneaker.size}
+              placeholder="Size"
+              name="size"
+            />
+            <input
+              className="w-full p-1 border-2 border-gray-200 rounded appearance-none"
+              type="text"
+              defaultValue={sneaker.imagePublicId}
+              placeholder="shoes/..."
+              name="image"
+            />
+            <input
+              className="w-full p-1 border-2 border-gray-200 rounded appearance-none"
+              type="number"
+              defaultValue={sneaker.price}
+              placeholder="Price"
+              name="price"
+            />
+            <input
+              className="w-full p-1 border-2 border-gray-200 rounded appearance-none"
+              type="number"
+              defaultValue={sneaker.retailPrice}
+              placeholder="Retail Price"
+              name="retailPrice"
+            />
+            <input
+              className="w-full p-1 border-2 border-gray-200 rounded appearance-none"
+              type="datetime-local"
+              defaultValue={format(parseISO(sneaker.purchaseDate), formatter)}
+              placeholder="Purchase Date"
+              name="purchaseDate"
+            />
+            <div
+              className="grid items-center w-full gap-2 sm:grid-cols-2 grid-col"
+              style={{
+                gridColumn: '1/3',
+                paddingTop: sold ? undefined : 6,
+              }}
+            >
+              <label className="flex items-center justify-between">
+                <span>Sold?</span>
+                <input
+                  type="checkbox"
+                  checked={sold}
+                  name="sold"
+                  onChange={event => setSold(event.currentTarget.checked)}
+                />
+              </label>
               <input
-                className="p-1 border-2 border-gray-200 rounded appearance-none"
-                type="text"
-                defaultValue={sneaker.brand.name}
-                placeholder="Brand"
-                name="brand"
-              />
-              <input
-                className="p-1 border-2 border-gray-200 rounded appearance-none"
-                type="text"
-                defaultValue={sneaker.model}
-                placeholder="Model"
-                name="model"
-              />
-              <input
-                className="p-1 border-2 border-gray-200 rounded appearance-none"
-                type="text"
-                defaultValue={sneaker.colorway}
-                placeholder="Colorway"
-                name="colorway"
-              />
-              <input
-                className="p-1 border-2 border-gray-200 rounded appearance-none"
-                type="number"
-                defaultValue={sneaker.size}
-                placeholder="Size"
-                name="size"
-              />
-              <input
-                className="p-1 border-2 border-gray-200 rounded appearance-none"
-                type="text"
-                defaultValue={sneaker.imagePublicId}
-                placeholder="shoes/..."
-                name="image"
-              />
-              <input
-                className="p-1 border-2 border-gray-200 rounded appearance-none"
-                type="number"
-                defaultValue={sneaker.price}
-                placeholder="Price"
-                name="price"
-              />
-              <input
-                className="p-1 border-2 border-gray-200 rounded appearance-none"
-                type="number"
-                defaultValue={sneaker.retailPrice}
-                placeholder="Retail Price"
-                name="retailPrice"
-              />
-              <input
-                className="p-1 border-2 border-gray-200 rounded appearance-none"
+                className={clsx(
+                  'p-1 border-2 border-gray-200 rounded appearance-none',
+                  sold ? '' : 'hidden'
+                )}
                 type="datetime-local"
-                defaultValue={format(parseISO(sneaker.purchaseDate), formatter)}
-                placeholder="Purchase Date"
-                name="purchaseDate"
+                defaultValue={
+                  sneaker.soldDate &&
+                  format(parseISO(sneaker.soldDate), formatter)
+                }
+                placeholder="Sold Date"
+                name="soldDate"
+                min={format(parseISO(sneaker.purchaseDate), formatter)}
               />
-              <div
-                className="grid items-center w-full gap-2 sm:grid-cols-2 grid-col"
-                style={{
-                  gridColumn: '1/3',
-                  paddingTop: sold ? undefined : 6,
-                }}
-              >
-                <label className="flex items-center justify-between">
-                  <span>Sold?</span>
-                  <input
-                    type="checkbox"
-                    checked={sold}
-                    name="sold"
-                    onChange={event => setSold(event.currentTarget.checked)}
-                  />
-                </label>
-                <input
-                  className={clsx(
-                    'p-1 border-2 border-gray-200 rounded appearance-none',
-                    sold ? '' : 'hidden'
-                  )}
-                  type="datetime-local"
-                  defaultValue={
-                    sneaker.soldDate &&
-                    format(parseISO(sneaker.soldDate), formatter)
-                  }
-                  placeholder="Sold Date"
-                  name="soldDate"
-                  min={format(parseISO(sneaker.purchaseDate), formatter)}
-                />
-                <input
-                  className={clsx(
-                    'p-1 border-2 border-gray-200 rounded appearance-none',
-                    sold ? '' : 'hidden'
-                  )}
-                  type="number"
-                  defaultValue={sneaker.soldPrice ?? undefined}
-                  placeholder="Sold Price"
-                  name="soldPrice"
-                />
-              </div>
+              <input
+                className={clsx(
+                  'p-1 border-2 border-gray-200 rounded appearance-none',
+                  sold ? '' : 'hidden'
+                )}
+                type="number"
+                defaultValue={sneaker.soldPrice ?? undefined}
+                placeholder="Sold Price"
+                name="soldPrice"
+              />
             </div>
             <button
               type="submit"
-              className="self-start w-auto px-4 py-2 text-left text-white bg-blue-500 rounded disabled:bg-blue-200 disabled:cursor-not-allowed"
+              className="self-start w-auto px-4 py-2 text-center text-white bg-blue-500 rounded disabled:bg-blue-200 disabled:cursor-not-allowed sm:col-span-2"
             >
               Sav{pendingForm ? 'ing' : 'e'} Changes
             </button>
