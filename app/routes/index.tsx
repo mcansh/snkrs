6700e19b34f3087413afed47618b49cdefdb245d
Logan McAnsh <logan@mcan.sh>
2021-06-07 22:20:04 -0400

    chore(IndexPage): only write session if session exists and has userId that doesnt exist
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,27 +3,32 @@ import { redirect } from 'remix';
 import { flashMessageKey, sessionKey } from '../constants';
 import { prisma } from '../db';
 import { flashMessage } from '../flash-message';
-import { withSession } from '../lib/with-session';
+import { commitSession, getSession } from '../session';
 
 import type { LoaderFunction } from 'remix';
 
-const loader: LoaderFunction = ({ request }) =>
-  withSession(request, async session => {
-    const userId = session.get(sessionKey);
+const loader: LoaderFunction = async ({ request }) => {
+  const session = await getSession(request.headers.get('Cookie'));
+  const userId = session.get(sessionKey);
 
-    if (userId) {
-      const user = await prisma.user.findUnique({ where: { id: userId } });
+  if (userId) {
+    const user = await prisma.user.findUnique({ where: { id: userId } });
 
-      if (user) {
-        return redirect(`/${user.username}`);
-      }
-
-      session.flash(flashMessageKey, flashMessage('User not found', 'error'));
-      return redirect(`/loganmcansh`);
+    if (user) {
+      return redirect(`/${user.username}`);
     }
 
-    return redirect(`/loganmcansh`);
-  });
+    session.flash(flashMessageKey, flashMessage('User not found', 'error'));
+    session.unset(sessionKey);
+    return redirect(`/loganmcansh`, {
+      headers: {
+        'Set-Cookie': await commitSession(session),
+      },
+    });
+  }
+
+  return redirect(`/loganmcansh`);
+};
 
 const IndexPage = () => null;
 
