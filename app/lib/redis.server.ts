4bd4087a10c059523b6d37f80b4a05c34a2756d5
Logan McAnsh <logan@mcan.sh>
2021-08-03 12:59:45 -0400

    chore(redis): cache for 10 seconds in development
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/lib/redis.server.ts
+++ app/lib/redis.server.ts
@@ -2,9 +2,15 @@ import Redis from 'ioredis';
 
 const redis = new Redis(process.env.FLY_REDIS_CACHE_URL);
 
+const isDev = process.env.NODE_ENV === 'development';
 async function saveByPage<T>(page: string, data: T, msTTL: number) {
   try {
-    return await redis.set(page, JSON.stringify(data), 'px', msTTL);
+    return await redis.set(
+      page,
+      JSON.stringify(data),
+      'px',
+      isDev ? 10_000 : msTTL
+    );
   } catch (error: unknown) {
     console.error('Failed to save in redis', error);
     return null;
