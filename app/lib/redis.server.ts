54e7cbe5efd5215666eb045d63a6b019526e08f3
Logan McAnsh <logan@mcan.sh>
2021-08-04 10:02:24 -0400

    chore(redis): return void
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/lib/redis.server.ts
+++ app/lib/redis.server.ts
@@ -3,17 +3,15 @@ import Redis from 'ioredis';
 const redis = new Redis(process.env.FLY_REDIS_CACHE_URL);
 
 const isDev = process.env.NODE_ENV === 'development';
-async function saveByPage<T>(page: string, data: T, msTTL: number) {
+async function saveByPage<T>(
+  page: string,
+  data: T,
+  msTTL: number
+): Promise<void> {
   try {
-    return await redis.set(
-      page,
-      JSON.stringify(data),
-      'px',
-      isDev ? 10_000 : msTTL
-    );
+    await redis.set(page, JSON.stringify(data), 'px', isDev ? 10_000 : msTTL);
   } catch (error: unknown) {
     console.error('Failed to save in redis', error);
-    return null;
   }
 }
 
