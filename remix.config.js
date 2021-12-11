143c07010dd446aa185b9fad56cf609a2f6eae77
Logan McAnsh <logan@mcan.sh>
2021-12-11 14:19:14 -0500

    chore: use assetsBuildDirectory instead of publicBuildDirectory
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,9 @@
+/**
+ * @type {import('@remix-run/dev/config').AppConfig}
+ */
 module.exports = {
   appDirectory: './app',
-  browserBuildDirectory: './public/build',
+  assetsBuildDirectory: './public/build',
   publicPath: '/build/',
   serverBuildDirectory: './build',
   devServerPort: 8002,
