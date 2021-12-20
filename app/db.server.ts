169c18157da1f8d692686d611633760f3e72b6f2
Logan McAnsh <logan@mcan.sh>
2021-12-20 16:46:17 -0500

    feat: add redis cache middleware to prisma queries
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    (cherry picked from commit dec850332cebc16a7d6936471c4d49995eaa6934)

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,4 +1,9 @@
+/* eslint no-console: ["error", { allow: ["warn", "error", "info"] }] */
+import crypto from 'crypto';
+
 import { PrismaClient } from '@prisma/client';
+
+import { redis } from './lib/redis.server';
 
 declare global {
   // This prevents us from making multiple connections to the db when the
@@ -18,5 +23,43 @@ if (process.env.NODE_ENV === 'production') {
   }
   prisma = global.globalPrismaClient;
 }
+
+function createHash(input: string) {
+  const sha1 = crypto.createHash('sha1');
+  const data = sha1.update(input, 'utf-8');
+  return data.digest('hex');
+}
+
+prisma.$use(async (params, next) => {
+  const queryTypes = [
+    'findUnique',
+    'findMany',
+    'findFirst',
+    'queryRaw',
+    'aggregate',
+  ];
+
+  if (!queryTypes.includes(params.action)) {
+    console.info(`Cache bypassed due to ${params.action} not being a query`);
+    return next(params);
+  }
+
+  const args = JSON.stringify(params.args);
+
+  const queryHash = createHash(`${params.model}_${params.action}_${args}`);
+  const cacheKey = `prisma:${queryHash}`;
+
+  const result = await redis.get(cacheKey);
+
+  if (result) {
+    console.info(`Cache hit for ${cacheKey}`);
+    return JSON.parse(result);
+  }
+
+  console.info(`Cache miss for ${cacheKey}`);
+  const newResult = await next(params);
+  await redis.set(cacheKey, JSON.stringify(newResult));
+  return newResult;
+});
 
 export { prisma };
