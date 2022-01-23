d3f6cc9970318d8fec847be5f5c737ab050fd262
Logan McAnsh <logan@mcan.sh>
2022-01-22 22:46:34 -0500

    chore: remove prisma redis cache
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,9 +1,4 @@
-/* eslint no-console: ["error", { allow: ["warn", "error", "info"] }] */
-import crypto from 'crypto';
-
 import { PrismaClient } from '@prisma/client';
-
-import { redis } from './lib/redis.server';
 
 declare global {
   // This prevents us from making multiple connections to the db when the
@@ -23,43 +18,5 @@ if (process.env.NODE_ENV === 'production') {
   }
   prisma = global.globalPrismaClient;
 }
-
-function createHash(input: string) {
-  const sha1 = crypto.createHash('sha1');
-  const data = sha1.update(input, 'utf-8');
-  return data.digest('hex');
-}
-
-prisma.$use(async (params, next) => {
-  const queryTypes = [
-    'findUnique',
-    'findMany',
-    'findFirst',
-    'queryRaw',
-    'aggregate',
-  ];
-
-  if (!queryTypes.includes(params.action)) {
-    console.info(`Cache bypassed due to ${params.action} not being a query`);
-    return next(params);
-  }
-
-  const args = JSON.stringify(params.args);
-
-  const queryHash = createHash(`${params.model}_${params.action}_${args}`);
-  const cacheKey = `prisma:${queryHash}`;
-
-  const result = await redis.get(cacheKey);
-
-  if (result) {
-    console.info(`Cache hit for ${cacheKey}`);
-    return JSON.parse(result);
-  }
-
-  console.info(`Cache miss for ${cacheKey}`);
-  const newResult = await next(params);
-  await redis.set(cacheKey, JSON.stringify(newResult));
-  return newResult;
-});
 
 export { prisma };
