a240214e5854d75958aed146888786ed2f86abbf
Logan McAnsh <logan@mcan.sh>
2022-02-17 21:55:43 -0500

    chore: remove comment
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/lib/upload-image.server.ts
+++ app/lib/upload-image.server.ts
@@ -57,29 +57,3 @@ export function createUploadHandler(
     );
   };
 }
-
-// export async function uploadImage(file: File) {
-//   const body = new FormData();
-//   body.append('file', file, file.name);
-
-//   let url = new URL(
-//     `/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/images/v1`,
-//     'https://api.cloudflare.com'
-//   );
-
-//   const uploadPromise = await fetch(url.toString(), {
-//     method: 'POST',
-//     headers: {
-//       Authorization: `Bearer ${process.env.CLOUDFLARE_IMAGES_API_TOKEN}`,
-//     },
-//     body,
-//   });
-
-//   let response = await uploadPromise.json();
-
-//   console.log(response);
-
-//   const isFileUploaded = response.status === 'success';
-
-//   // ... File is uploaded to CF Images
-// }
