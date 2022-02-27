c421ff510f9ade6b6f5196cd2d2d6dd534167fb8
Logan McAnsh <logan@mcan.sh>
2022-02-27 13:35:11 -0500

    feat: use litestream
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/lib/redis.server.ts
+++ app/lib/redis.server.ts
@@ -4,6 +4,22 @@ if (!process.env.FLY_REDIS_CACHE_URL) {
   throw new Error('FLY_REDIS_CACHE_URL is not defined');
 }
 
-const redis = new Redis(process.env.FLY_REDIS_CACHE_URL);
+declare global {
+  // This prevents us from making multiple connections to redis when the
+  // require cache is cleared.
+  // eslint-disable-next-line vars-on-top, no-var
+  var globalRedisClient: Redis.Redis | undefined;
+}
+
+let redis: Redis.Redis;
+
+if (process.env.NODE_ENV === 'production') {
+  redis = new Redis(process.env.FLY_REDIS_CACHE_URL);
+} else {
+  if (!global.globalRedisClient) {
+    global.globalRedisClient = new Redis(process.env.FLY_REDIS_CACHE_URL);
+  }
+  redis = global.globalRedisClient;
+}
 
 export { redis };
