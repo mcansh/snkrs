5ab28c994899439c31ac4f0b2b9450a6719eaf60
Logan McAnsh <logan@mcan.sh>
2022-02-28 21:41:44 -0500

    chore: add logout ui
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,18 +1,39 @@
-import { redirect } from 'remix';
-import type { LoaderFunction } from 'remix';
+import { Form, json, redirect } from 'remix';
+import type { ActionFunction, LoaderFunction } from 'remix';
 
 import { destroySession, getSession } from '~/session';
+import { sessionKey } from '~/constants';
 
-const loader: LoaderFunction = async ({ request }) => {
-  const session = await getSession(request.headers.get('Cookie'));
-  return redirect('/login', {
+export let loader: LoaderFunction = async ({ request }) => {
+  let session = await getSession(request.headers.get('Cookie'));
+  if (!session.has(sessionKey)) {
+    return redirect('/');
+  }
+  return json(null);
+};
+
+export let action: ActionFunction = async ({ request }) => {
+  let session = await getSession(request.headers.get('Cookie'));
+  return redirect('/', {
     headers: {
       'Set-Cookie': await destroySession(session),
     },
   });
 };
 
-const LogoutPage = () => null;
-
-export default LogoutPage;
-export { loader };
+export default function LogoutPage() {
+  return (
+    <Form
+      reloadDocument
+      method="post"
+      className="h-full grid place-items-center min-h-screen"
+    >
+      <button
+        className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+        type="submit"
+      >
+        Log out
+      </button>
+    </Form>
+  );
+}
