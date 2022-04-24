9d0a29b6047ed29907d338fa24f05521c809b473
Logan McAnsh <logan@mcan.sh>
2022-04-24 15:09:02 -0400

    chore: update security headers config
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -3,27 +3,73 @@ import type { EntryContext, HandleDataRequestFunction } from '@remix-run/node';
 import { redirect } from '@remix-run/node';
 import { RemixServer } from '@remix-run/react';
 import etag from 'etag';
+import { createSecureHeaders } from '@mcansh/remix-secure-headers';
 
-// https://securityheaders.com
-let cspSettings = {
-  'default-src': ["'self'"],
-  'img-src': [
-    "'self'",
-    'data:',
-    'https://dof0zryca-res.cloudinary.com',
-    'https://kiwi.mcan.sh',
-  ],
-  'style-src': ["'self'", "'unsafe-inline'"],
-  'script-src': ["'self'", "'unsafe-inline'", 'https://kiwi.mcan.sh/script.js'],
-  'connect-src': [
-    "'self'",
-    ...(process.env.NODE_ENV === 'production' ? [] : [`ws://localhost:8002`]),
-  ],
-};
-
-let contentSecurityPolicy = `${Object.entries(cspSettings)
-  .map(([key, val]) => `${key} ${val.filter(Boolean).join(' ')}`)
-  .join(';')}`;
+let securityHeaders = createSecureHeaders({
+  'Content-Security-Policy': {
+    'default-src': ["'self'"],
+    'img-src': [
+      "'self'",
+      'data:',
+      'https://dof0zryca-res.cloudinary.com',
+      'https://kiwi.mcan.sh',
+    ],
+    'style-src': ["'self'", "'unsafe-inline'"],
+    'script-src': [
+      "'self'",
+      "'unsafe-inline'",
+      'https://kiwi.mcan.sh/script.js',
+    ],
+    'connect-src':
+      process.env.NODE_ENV === 'production'
+        ? ["'self'"]
+        : ["'self'", `ws://localhost:${process.env.REMIX_DEV_SERVER_WS_PORT}`],
+  },
+  'Permissions-Policy': {
+    accelerometer: [],
+    ambientLightSensor: [],
+    autoplay: [],
+    battery: [],
+    camera: [],
+    displayCapture: [],
+    documentDomain: [],
+    encryptedMedia: [],
+    executionWhileNotRendered: [],
+    executionWhileOutOfViewport: [],
+    fullscreen: [],
+    gamepad: [],
+    geolocation: [],
+    gyroscope: [],
+    layoutAnimations: [],
+    legacyImageFormats: [],
+    magnetometer: [],
+    microphone: [],
+    midi: [],
+    navigationOverride: [],
+    oversizedImages: [],
+    payment: [],
+    pictureInPicture: [],
+    publickeyCredentialsGet: [],
+    speakerSelection: [],
+    syncXhr: [],
+    unoptimizedImages: [],
+    unsizedMedia: [],
+    usb: [],
+    screenWakeLock: [],
+    webShare: [],
+    xrSpatialTracking: [],
+  },
+  'Cross-Origin-Opener-Policy': 'same-origin',
+  'Referrer-Policy': 'strict-origin-when-cross-origin',
+  'Strict-Transport-Security': {
+    maxAge: 31536000,
+    includeSubDomains: true,
+    preload: true,
+  },
+  'X-Frame-Options': 'DENY',
+  'X-Content-Type-Options': 'nosniff',
+  'X-DNS-Prefetch-Control': 'on',
+});
 
 export default function handleDocumentRequest(
   request: Request,
@@ -52,30 +98,10 @@ export default function handleDocumentRequest(
   }
 
   responseHeaders.set('Content-Type', 'text/html');
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/ETag
   responseHeaders.set('ETag', markupETag);
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-  responseHeaders.set('Content-Security-Policy', contentSecurityPolicy);
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  responseHeaders.set('Referrer-Policy', 'strict-origin-when-cross-origin');
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-  responseHeaders.set('X-Frame-Options', 'DENY');
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-  responseHeaders.set('X-Content-Type-Options', 'nosniff');
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-  responseHeaders.set('X-DNS-Prefetch-Control', 'on');
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-  responseHeaders.set(
-    'Strict-Transport-Security',
-    'max-age=31536000; includeSubDomains; preload'
-  );
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-  responseHeaders.set(
-    'Permissions-Policy',
-    'camera=(), microphone=(), geolocation=()'
-  );
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cross-Origin-Opener-Policy
-  responseHeaders.set('Cross-Origin-Opener-Policy', 'same-origin');
+  for (let header of securityHeaders) {
+    responseHeaders.set(...header);
+  }
 
   if (process.env.FLY_REGION) {
     responseHeaders.set('X-Fly-Region', process.env.FLY_REGION);
