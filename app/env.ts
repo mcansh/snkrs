18f1c4e341f9255cdf5197da87786d0a8b282ef9
Logan McAnsh <logan@mcan.sh>
2023-02-27 11:01:19 -0500

    chore: add cloudinary to required env
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/env.ts
+++ app/env.ts
@@ -7,6 +7,10 @@ let envSchema = z.object({
   SESSION_PASSWORD: z.string(),
   NODE_ENV: z.enum(["development", "production", "test"]),
   DEFAULT_USER: z.string(),
+  CLOUDINARY_CLOUD_NAME: z.string(),
+  CLOUDINARY_UPLOAD_PRESET: z.string(),
+  CLOUDINARY_UPLOAD_URL: z.string(),
+  CLOUDINARY_URL: z.string(),
 });
 
 export let env = envSchema.parse(process.env);
